a78257b46d644f841da32d070672c28b6d0a05ed
Sufair Ahmed <sufair190@gmail.com>
2020-06-10 07:07:53 +0600

    even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+a = int(input("enter a valid number: "))
+
+if (a % 2 == 0):
+    # print("input is even")
+    print('{} is even'.format(a))
+else:
+    # print("the number is odd")
+        print('{} is odd'.format(a))
